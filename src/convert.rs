735f09b22b10ac276ab48b5076b6578d1c09d970
Austin Schey <aschey13@gmail.com>
2025-12-26 10:54:02 -0500

    feat!: ratatui 0.30 (#12)

--- src/convert.rs
+++ src/convert.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "termprofile")]
 use termprofile::TermProfile;
 
-/// Converts between [`syntect`] styles and [`ratatui`] styles.
+/// Converts between [`syntect`] styles and [`ratatui`](ratatui_core) styles.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Converter {
     #[cfg(feature = "termprofile")]
@@ -30,16 +30,16 @@ impl Converter {
     }
 
     /// Converts the syntect [`Style`](syntect::highlighting::Style) to a ratatui
-    /// [`Style`](ratatui::style::Style).
+    /// [`Style`](ratatui_core::style::Style).
     pub fn syntect_style_to_tui(
         &self,
         style: syntect::highlighting::Style,
-    ) -> ratatui::style::Style {
+    ) -> ratatui_core::style::Style {
         #[cfg(feature = "termprofile")]
         if self.profile == termprofile::TermProfile::NoTty {
-            return ratatui::style::Style::new();
+            return ratatui_core::style::Style::new();
         }
-        let mut tui_style = ratatui::style::Style::new();
+        let mut tui_style = ratatui_core::style::Style::new();
         if let Some(fg) = self.syntect_color_to_tui(style.foreground) {
             tui_style = tui_style.fg(fg);
         }
@@ -49,31 +49,31 @@ impl Converter {
         tui_style.add_modifier(syntect_modifiers_to_tui(&style.font_style))
     }
 
-    /// Converts the syntect [`Color`](ratatui::style::Color) to a ratatui
-    /// [`Color`](ratatui::style::Color).
+    /// Converts the syntect [`Color`](ratatui_core::style::Color) to a ratatui
+    /// [`Color`](ratatui_core::style::Color).
     pub fn syntect_color_to_tui(
         &self,
         color: syntect::highlighting::Color,
-    ) -> Option<ratatui::style::Color> {
+    ) -> Option<ratatui_core::style::Color> {
         if color.a == 0 {
             Some(match color.r {
-                0x00 => ratatui::style::Color::Black,
-                0x01 => ratatui::style::Color::Red,
-                0x02 => ratatui::style::Color::Green,
-                0x03 => ratatui::style::Color::Yellow,
-                0x04 => ratatui::style::Color::Blue,
-                0x05 => ratatui::style::Color::Magenta,
-                0x06 => ratatui::style::Color::Cyan,
-                0x07 => ratatui::style::Color::Gray,
-                0x08 => ratatui::style::Color::DarkGray,
-                0x09 => ratatui::style::Color::LightRed,
-                0x0A => ratatui::style::Color::LightGreen,
-                0x0B => ratatui::style::Color::LightYellow,
-                0x0C => ratatui::style::Color::LightBlue,
-                0x0D => ratatui::style::Color::LightMagenta,
-                0x0E => ratatui::style::Color::LightCyan,
-                0x0F => ratatui::style::Color::White,
-                c => ratatui::style::Color::Indexed(c),
+                0x00 => ratatui_core::style::Color::Black,
+                0x01 => ratatui_core::style::Color::Red,
+                0x02 => ratatui_core::style::Color::Green,
+                0x03 => ratatui_core::style::Color::Yellow,
+                0x04 => ratatui_core::style::Color::Blue,
+                0x05 => ratatui_core::style::Color::Magenta,
+                0x06 => ratatui_core::style::Color::Cyan,
+                0x07 => ratatui_core::style::Color::Gray,
+                0x08 => ratatui_core::style::Color::DarkGray,
+                0x09 => ratatui_core::style::Color::LightRed,
+                0x0A => ratatui_core::style::Color::LightGreen,
+                0x0B => ratatui_core::style::Color::LightYellow,
+                0x0C => ratatui_core::style::Color::LightBlue,
+                0x0D => ratatui_core::style::Color::LightMagenta,
+                0x0E => ratatui_core::style::Color::LightCyan,
+                0x0F => ratatui_core::style::Color::White,
+                c => ratatui_core::style::Color::Indexed(c),
             })
         } else if color.a == 1 {
             None
@@ -81,23 +81,25 @@ impl Converter {
             #[cfg(feature = "termprofile")]
             return self
                 .profile
-                .adapt_color(ratatui::style::Color::Rgb(color.r, color.g, color.b));
+                .adapt_color(ratatui_core::style::Color::Rgb(color.r, color.g, color.b));
             #[cfg(not(feature = "termprofile"))]
-            return Some(ratatui::style::Color::Rgb(color.r, color.g, color.b));
+            return Some(ratatui_core::style::Color::Rgb(color.r, color.g, color.b));
         }
     }
 }
 
-fn syntect_modifiers_to_tui(style: &syntect::highlighting::FontStyle) -> ratatui::style::Modifier {
-    let mut modifier = ratatui::style::Modifier::empty();
+fn syntect_modifiers_to_tui(
+    style: &syntect::highlighting::FontStyle,
+) -> ratatui_core::style::Modifier {
+    let mut modifier = ratatui_core::style::Modifier::empty();
     if style.intersects(syntect::highlighting::FontStyle::BOLD) {
-        modifier |= ratatui::style::Modifier::BOLD;
+        modifier |= ratatui_core::style::Modifier::BOLD;
     }
     if style.intersects(syntect::highlighting::FontStyle::ITALIC) {
-        modifier |= ratatui::style::Modifier::ITALIC;
+        modifier |= ratatui_core::style::Modifier::ITALIC;
     }
     if style.intersects(syntect::highlighting::FontStyle::UNDERLINE) {
-        modifier |= ratatui::style::Modifier::UNDERLINED;
+        modifier |= ratatui_core::style::Modifier::UNDERLINED;
     }
     modifier
 }
